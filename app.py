54dd18f708dbc4f57e493ae2c6c30dfa4a0fd4e7
“gtla6224422” <nonop01@163.com>
2024-08-27 09:31:50 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/api/data', methods=['POST'])
+def post_data():
+    data = request.get_json()
+    if 'name' in data:
+        response = {'message': f'Hello, {data["name"]}!'}
+    else:
+        response = {'error': 'Missing name in the request.'}
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
