5064f1dd11538e9888e7adc1d20c40a9c310a706
“gtla6224422” <nonop01@163.com>
2024-09-19 15:26:08 +0800

    del main.py

--- backend/Test_mini.py
+++ backend/Test_mini.py
@@ -23,4 +23,4 @@ def create_data():
     return jsonify(new_data), 201
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000,debug=True)
+    app.run(host='0.0.0.0', port=5001,debug=True)
